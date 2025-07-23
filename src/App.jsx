890ca054515d7cd8b40fad76d0ac33c41a8851b2
crisprtech <orachadongo@gmail.com>
2025-07-23 21:34:18 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Header from "../src/pages/landingPage/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EducationalPlatform from "./pages/ResearchRepo/Educational";
